051821a0da6f30d66131ad7cb7f4789a3283665e
projectskl17 <hermit0bot@gmail.com>
2024-10-06 23:23:10 +0530

    sub

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -9,20 +9,37 @@ from Adarsh.vars import Var
 from urllib.parse import quote_plus
 from pyrogram import filters, Client
 from pyrogram.errors import FloodWait, UserNotParticipant
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ChatMemberStatus
 
 from Adarsh.utils.file_properties import get_name, get_hash, get_media_file_size
 db = Database(Var.DATABASE_URL, Var.name)
-
 
 MY_PASS = os.environ.get("MY_PASS", None)
 pass_dict = {}
 pass_db = Database(Var.DATABASE_URL, "ag_passwords")
 
+FORCE_SUB_CHANNEL = os.environ.get("FORCE_SUB_CHANNEL", None)
+ADMINS = list(map(int, os.environ.get("ADMINS", "").split()))
 
+async def is_subscribed(filter, client, update):
+    if not FORCE_SUB_CHANNEL:
+        return True
+    user_id = update.from_user.id
+    if user_id in ADMINS:
+        return True
+    try:
+        member = await client.get_chat_member(chat_id=FORCE_SUB_CHANNEL, user_id=user_id)
+    except UserNotParticipant:
+        return False
+    
+    if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]:
+        return False
+    else:
+        return True
 
+subscribed = filters.create(is_subscribed)
 
-@StreamBot.on_message((filters.private) & (filters.document | filters.video | filters.audio | filters.photo) , group=4)
+@StreamBot.on_message((filters.private) & (filters.document | filters.video | filters.audio | filters.photo))
 async def private_receive_handler(c: Client, m: Message):
     if not await db.is_user_exist(m.from_user.id):
         await db.add_user(m.from_user.id)
@@ -30,6 +47,11 @@ async def private_receive_handler(c: Client, m: Message):
             Var.BIN_CHANNEL,
             f"New User Joined! : \n\n Name : [{m.from_user.first_name}](tg://user?id={m.from_user.id}) Started Your Bot!!"
         )
+    
+    if not await is_subscribed(None, c, m):
+        await handle_not_subscribed(c, m)
+        return
+    
     try:
         log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
         stream_link = f"{Var.URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
@@ -43,28 +65,41 @@ async def private_receive_handler(c: Client, m: Message):
 <b>Watch:</b> <i>{}</i>
 """
 
-
         await log_msg.reply_text(
-    text=f"""\
+            text=f"""\
 **Requested by:** [{m.from_user.first_name}](tg://user?id={m.from_user.id})  
 **User ID:** `{m.from_user.id}`  
 **Stream Link:** {stream_link}
 """,
-    disable_web_page_preview=True,
-    quote=True
-)
+            disable_web_page_preview=True,
+            quote=True
+        )
 
         await m.reply_text(
             text=msg_text.format(get_name(log_msg), humanbytes(get_media_file_size(m)), online_link, stream_link),
             quote=True,
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Stream", url=stream_link), #Stream Link
-                                                InlineKeyboardButton("Download", url=online_link)]]) #Download Link
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Stream", url=stream_link),
+                                                InlineKeyboardButton("Download", url=online_link)]])
         )
     except FloodWait as e:
         print(f"Sleeping for {str(e.x)}s")
         await asyncio.sleep(e.x)
         await c.send_message(chat_id=Var.BIN_CHANNEL, text=f"Gᴏᴛ FʟᴏᴏᴅWᴀɪᴛ ᴏғ {str(e.x)}s from [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n\n**𝚄𝚜𝚎𝚛 𝙸𝙳 :** `{str(m.from_user.id)}`", disable_web_page_preview=True)
+
+async def handle_not_subscribed(client, message):
+    buttons = [
+        [
+            InlineKeyboardButton("Join Channel", url=f"https://t.me/{FORCE_SUB_CHANNEL}")
+        ]
+    ]
+    text = "**Please join our channel to use this bot!**\n\nClick the button below to join:"
+    await message.reply_text(
+        text=text,
+        reply_markup=InlineKeyboardMarkup(buttons),
+        quote=True,
+        disable_web_page_preview=True
+    )
 
 
 @StreamBot.on_message(filters.channel & ~filters.group & (filters.document | filters.video | filters.photo)  & ~filters.forwarded, group=-1)
